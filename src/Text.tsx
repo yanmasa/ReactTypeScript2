9410b488cc7671f22808152d49d23794530a2f84
Masaya Ueda <88300005+yanmasa@users.noreply.github.com>
2022-05-18 11:56:49 +0900

    VFC

--- src/Text.tsx
+++ src/Text.tsx
@@ -1,11 +1,11 @@
-import { FC } from "react";
+import { VFC } from "react";
 
 type textStyle = {
   color: string;
   fontSize: string;
 };
 
-export const Text: FC<textStyle> = (props) => {
-  const { color, fontSize, children } = props;
-  return <p style={{ color, fontSize }}>{children}</p>;
+export const Text: VFC<textStyle> = (props) => {
+  const { color, fontSize } = props;
+  return <p style={{ color, fontSize }}>text</p>;
 };
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,7 +1,8 @@
+import { VFC } from "react";
 import { TodoType } from "./types/todo";
 
-export const Todo = (
-  props: Pick<TodoType, "title" | "userId" | "completed">
+export const Todo: VFC<Pick<TodoType, "title" | "userId" | "completed">> = (
+  props
 ) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
