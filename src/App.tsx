1a6fe923493412a820d544899b6c4e98bc728408
Masaya Ueda <88300005+yanmasa@users.noreply.github.com>
2022-05-18 11:01:32 +0900

    noformatbug

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Todo } from "./Todo";
+
+import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      setTodos(res.data);
+    });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
